6648b8dc24c533ec327db51af99bfc54eda88dcc
=giahy26052004 <=giahy26052004@gmail.com>
2025-05-23 10:30:32 +0700

    first commit

--- src/server.js
+++ src/server.js
@@ -18,5 +18,5 @@ mongoose
 // Start server
 const port = process.env.PORT || 4000;
 app.listen(port, "0.0.0.0", () => {
-  console.log(`Server is running on http://0.0.0.0:${PORT}`);
+  console.log(`Server is running on http://0.0.0.0:${port}`);
 });
